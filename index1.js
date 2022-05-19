ac44942cb3a15cbc442161e6b56b24111581ac53
tinahtina <84130405+tinahtina@users.noreply.github.com>
2022-05-19 10:26:00 +0300

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,16 @@
+
+import Header from "../frontie/Hero";
+import Layout from "../frontie/Layout"
+
+const home = () => {
+
+    return(
+        <>
+        <Layout>
+            <Header/>
+        </Layout>
+        
+        </>
+    )
+}
+export default home
